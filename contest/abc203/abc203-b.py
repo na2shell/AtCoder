e796949ad8c4229b27d6c75528cbde1900929e33
na2 <51122019+na2shell@users.noreply.github.com>
2021-09-04 21:57:39 +0900

    solve abc203 b

---
+++ contest/abc203/abc203-b.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Your code here!
+n,k = map(int,input().split())
+ans = 0
+
+for i in range(1,n+1):
+  for j in range(1,k+1):
+    num = int(str(i)+"0"+str(j))
+    ans += num
+
+print(ans)
+
+
